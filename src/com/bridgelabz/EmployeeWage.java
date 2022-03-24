09f3e2d63298cc95175cf91d0b824116da1e08f4
Dhanshri911 <dszingade911@gmail.com>
2022-03-24 11:31:49 +0530

    UC1:Added Employee is Present or not

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -4,7 +4,13 @@ public class EmployeeWage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Welcome..");
+		int empIsPresent = (int) (Math.floor(Math.random() * 10) % 2);
+		if (empIsPresent == 1) {
+			System.out.println("Employee is Present");
+		} else {
+			System.out.println("Employee is Absent");
+		}
+
 	}
 
 }
